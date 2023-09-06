c920a4bb31c64cf72dc12350291200307a4c3e4d
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2023-09-06 10:56:26 -0400

    prep work for dd testing setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1234,7 +1234,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -55, -15, -1.17 }
+#define NOZZLE_TO_PROBE_OFFSET { -55, -15, -0.92 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
