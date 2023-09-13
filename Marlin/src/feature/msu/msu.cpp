d68cf66d4ed95f8a9fd25f8fd824032dc83894b3
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2023-09-12 23:57:17 -0400

    :bug: direct drive concurent extruder move bugfix

--- Marlin/src/feature/msu/msu.cpp
+++ Marlin/src/feature/msu/msu.cpp
@@ -17,57 +17,53 @@ xyze_pos_t position;
 
 float steps_per_mm_correction_factor = 1;
 #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-steps_per_mm_correction_factor = MSU_EXTRUDER_STEPS_PER_MM/static_cast<float>(planner.settings.axis_steps_per_mm[E_AXIS]);
+  steps_per_mm_correction_factor = MSU_EXTRUDER_STEPS_PER_MM / static_cast<float>(planner.settings.axis_steps_per_mm[E_AXIS]);
 #endif
 void MSUMP::tool_change(uint8_t index)
 {
-  #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-    move_extruder(-MSU_GEAR_LENGTH,MSU_SPEED);
-  #endif
 
-  #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-    move_extruder(-MSU_GEAR_LENGTH,MSU_SPEED);
-  #endif
+#if ENABLED(MSU_DIRECT_DRIVE_SETUP)
+  move_extruder(-MSU_GEAR_LENGTH, MSU_SPEED, MSU_ORIGINAL_EXTRUDER_NBR);
+#endif
+
+#if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
+  move_extruder(-MSU_GEAR_LENGTH, MSU_SPEED, MSU_EXTRUDER_NBR);
+#endif
 
   idler_select_filament_nbr(selected_filament_nbr);
-  move_extruder(-MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,MSU_SPEED);
+  move_extruder(-MSU_BOWDEN_TUBE_LENGTH * steps_per_mm_correction_factor, MSU_SPEED, MSU_EXTRUDER_NBR);
   idler_select_filament_nbr(index);
-  selected_filament_nbr=index;
-  move_extruder(MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,MSU_SPEED);
+  selected_filament_nbr = index;
+  move_extruder(MSU_BOWDEN_TUBE_LENGTH * steps_per_mm_correction_factor, MSU_SPEED, MSU_EXTRUDER_NBR);
 
   #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-    move_extruder(4,MSU_SPEED);
+    move_extruder(4,MSU_SPEED,true);
     idler_select_filament_nbr(-1);
-    move_extruder(MSU_GEAR_LENGTH,MSU_SPEED);
+    move_extruder(MSU_GEAR_LENGTH,MSU_SPEED,true);
   #endif
 
-  #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-    move_extruder(4,MSU_SPEED);
-    idler_select_filament_nbr(-1);
-    move_extruder(MSU_GEAR_LENGTH,MSU_SPEED);
-  #endif
+#if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
+  idler_select_filament_nbr(-1);
+  move_extruder(MSU_GEAR_LENGTH, MSU_SPEED, MSU_EXTRUDER_NBR);
+#endif
 }
 
-void MSUMP::move_extruder(float dist, const_feedRate_t speed, bool moveBothExtruders)
+void MSUMP::move_both_extruders(float dist, const_feedRate_t speed)
 {
+  move_extruder(dist, speed, MSU_EXTRUDER_NBR, false);         // queue first extruder move
+  move_extruder(dist, speed, MSU_ORIGINAL_EXTRUDER_NBR, true); // second extruder move that waits for planner sync
+}
+
+void MSUMP::move_extruder(float dist, const_feedRate_t speed, int extruder_nbr, bool sync)
+{
+  SERIAL_ECHO_MSG("MSU: move_extruder: ", dist, " ", speed, "extruder_nbr: ", extruder_nbr);
   const float old = current_position.e;
   current_position.e += dist;
-  planner.buffer_line(current_position, speed, MSU_EXTRUDER_NBR);
+  planner.buffer_line(current_position, speed, extruder_nbr);
   current_position.e = old;
   planner.set_e_position_mm(old);
-
-  #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-    if (moveBothExtruders)
-    {
-      const float old = current_position.e;
-      current_position.e += dist;
-      planner.buffer_line(current_position, speed, MSU_ORIGINAL_EXTRUDER_NBR);
-      current_position.e = old;
-      planner.set_e_position_mm(old);
-    }
-  #endif
-  
-  planner.synchronize();
+  if (sync)
+    planner.synchronize();
 }
 
 // move idler to specific filament selection, -1 to park the idler
